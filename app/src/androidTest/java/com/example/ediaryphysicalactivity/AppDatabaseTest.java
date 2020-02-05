617231d8763e66db09e26fde449318207adbe241
Florian Meinhart <florianmeinhart@gmx.at>
2020-02-05 23:36:23 +0100

    Weather API and GPS working

---
+++ app/src/androidTest/java/com/example/ediaryphysicalactivity/AppDatabaseTest.java
@@ -0,0 +1,7 @@
+package com.example.ediaryphysicalactivity;
+
+import static org.junit.Assert.*;
+
+public class AppDatabaseTest {
+
+}
--- app/src/main/java/com/example/ediaryphysicalactivity/WeatherActivity.java
+++ app/src/main/java/com/example/ediaryphysicalactivity/WeatherActivity.java
@@ -1,11 +1,22 @@
 package com.example.ediaryphysicalactivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import android.os.AsyncTask;
+import android.provider.Settings;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -15,6 +26,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.DateFormat;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -27,6 +39,19 @@ public class WeatherActivity extends AppCompatActivity {
 
     TextView addressTxt, updated_atTxt, statusTxt, tempTxt, temp_minTxt, temp_maxTxt, sunriseTxt,
             sunsetTxt, windTxt, pressureTxt, humidityTxt;
+
+    TextView textViewLocation;
+    Button btnGetLocation;
+    Button btnUpdateWeather;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    String latitude = "47.81119500";
+    String longitude = "13.03322900";
+    DecimalFormat decimalFormat = new DecimalFormat("#.00000000");
+
+    Boolean buttonFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +71,53 @@ public class WeatherActivity extends AppCompatActivity {
         humidityTxt = findViewById(R.id.humidity);
 
         new weatherTask().execute();
+
+
+        textViewLocation = findViewById(R.id.text_view_location);
+        textViewLocation.setText("Lat:  " + latitude + "  \nLon: " + longitude);
+
+        btnGetLocation = findViewById(R.id.button_get_location);
+        btnGetLocation.setText("Start GPS");
+
+        btnUpdateWeather = findViewById(R.id.button_update_weather);
+        btnUpdateWeather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new weatherTask().execute();
+            }
+        });
+
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                latitude = decimalFormat.format(location.getLatitude());
+                longitude = decimalFormat.format(location.getLongitude());
+                textViewLocation.setText("Lat:  " + latitude + "  \nLon: " + longitude);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        configure_button();
     }
 
     class weatherTask extends AsyncTask<String, Void, String> {
@@ -60,13 +132,17 @@ public class WeatherActivity extends AppCompatActivity {
         }
 
         protected String doInBackground(String... args) {
-            String response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=" + API);
+            String targetURL = "api.openweathermap.org/data/2.5/weather?lat=35&lon=139";
+
+            //String response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=" + API);
+            String response = HttpRequest
+                    .excuteGet("https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude
+                            + "&units=metric&appid=" + API);
             return response;
         }
 
         @Override
         protected void onPostExecute(String result) {
-
 
             DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
             DateFormat dateFormatTimeOnly = new SimpleDateFormat("HH:mm");
@@ -104,9 +180,9 @@ public class WeatherActivity extends AppCompatActivity {
                 temp_maxTxt.setText(tempMax);
                 sunriseTxt.setText(dateFormatTimeOnly.format(sunrise * 1000));
                 sunsetTxt.setText(dateFormatTimeOnly.format(sunset * 1000));
-                windTxt.setText(windSpeed);
-                pressureTxt.setText(pressure);
-                humidityTxt.setText(humidity);
+                windTxt.setText(windSpeed + " m/s");
+                pressureTxt.setText(pressure + " hpa");
+                humidityTxt.setText(humidity + "%");
 
                 /* Views populated, Hiding the loader, Showing the main design */
                 findViewById(R.id.loader).setVisibility(View.GONE);
@@ -120,4 +196,47 @@ public class WeatherActivity extends AppCompatActivity {
 
         }
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}, 10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        btnGetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!buttonFlag) {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        locationManager.requestLocationUpdates("gps", 1000, 0, locationListener);
+                    }
+                    btnGetLocation.setText("Stop GPS");
+                    buttonFlag = true;
+                } else {
+                    locationManager.removeUpdates(locationListener);
+                    btnGetLocation.setText("Start GPS");
+                    buttonFlag = false;
+                }
+
+            }
+        });
+    }
+
 }
